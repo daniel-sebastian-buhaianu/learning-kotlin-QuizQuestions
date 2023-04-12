23d66b21a7bc7aff890f0842e8079f31e44cf20c
Daniel-Sebastian Buhaianu <dsb99.dev@gmail.com>
2023-04-12 07:44:39 +0300

    organised code better

--- quiz.kt
+++ quiz.kt
@@ -1,22 +1,24 @@
-// class FillInTheBlankQuestion(
-//     val questionText: String,
-//     val answer: String,
-//     val difficulty: String
-// )
+/*
+class FillInTheBlankQuestion(
+    val questionText: String,
+    val answer: String,
+    val difficulty: String
+)
 
-// class TrueOrFalseQuestion(
-//     val questionText: String,
-//     val answer: Boolean,
-//     val difficulty: String
-// )
+class TrueOrFalseQuestion(
+    val questionText: String,
+    val answer: Boolean,
+    val difficulty: String
+)
 
-// class NumericQuestion(
-//     val questionText: String,
-//     val answer: Int,
-//     val difficulty: String
-// )
+class NumericQuestion(
+    val questionText: String,
+    val answer: Int,
+    val difficulty: String
+)
+*/
 
-class Question<T>(
+data class Question<T>(
     val questionText: String,
     val answer: T,
     val difficulty: Difficulty
@@ -26,8 +28,17 @@ enum class Difficulty {
     EASY, MEDIUM, HARD
 }
 
-fun main() {
+class Quiz {
     val question1 = Question<String>("Quoth the raven ___", "nevermore", Difficulty.MEDIUM)
     val question2 = Question<Boolean>("The sky is green. True or false", false, Difficulty.EASY)
     val question3 = Question<Int>("How many days are there between full moons?", 28, Difficulty.HARD)
+
+    companion object StudentProgress {
+        var total: Int = 10
+        var answered: Int = 3
+    }
+}
+
+fun main() {
+    println("${Quiz.answered} of ${Quiz.total} questions answered so far.")
 }
