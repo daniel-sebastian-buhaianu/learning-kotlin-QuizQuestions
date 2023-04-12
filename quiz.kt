4da57f5ae295590a36f1f2d4aa0b925eee7f493f
Daniel-Sebastian Buhaianu <dsb99.dev@gmail.com>
2023-04-12 08:51:02 +0300

    refactored code and improved design

--- quiz.kt
+++ quiz.kt
@@ -1,5 +1,7 @@
+import kotlin.math.round
+
 fun main() {
-    Quiz.printProgressBar()
+    Quiz().printProgressBar()
 }
 
 /*
@@ -32,23 +34,34 @@ enum class Difficulty {
     EASY, MEDIUM, HARD
 }
 
-class Quiz {
+interface ProgressPrintable {
+    val progressText: String
+    fun printProgressBar()
+}
+
+class Quiz : ProgressPrintable {
     val question1 = Question<String>("Quoth the raven ___", "nevermore", Difficulty.MEDIUM)
     val question2 = Question<Boolean>("The sky is green. True or false", false, Difficulty.EASY)
     val question3 = Question<Int>("How many days are there between full moons?", 28, Difficulty.HARD)
 
     companion object StudentProgress {
         var total: Int = 10
-        var answered: Int = 3
+        var answered: Int = 5
     }
-}
 
-val Quiz.StudentProgress.progressText: String
-    get() = "${answered} of ${total} questions answered"
+    override val progressText: String
+        get() = "${answered} of ${total} questions answered"
 
-fun Quiz.StudentProgress.printProgressBar() {
-    repeat(Quiz.answered) { print("▓") }
-    repeat(Quiz.total - Quiz.answered) { print("▒") }
-    println()
-    println(Quiz.progressText)
+    override fun printProgressBar() {
+        val progressBarLength: Double = progressText.length.toDouble()
+        val factor: Double = progressBarLength / total
+        val currentAnswers = round(answered * factor).toInt()
+        val remainingAnswers = (progressBarLength - round(answered * factor)).toInt()
+
+        repeat(currentAnswers) { print("▓") }
+        repeat(remainingAnswers) { print("▒") }
+
+        println()
+        println(progressText)
+    }
 }
